a5a464e7e59eac37ac2620aff71362e7d44ac060
Mohammadreza Zarif <m.zariffard1381@gmail.com>
2025-05-18 20:43:32 +0300

    feat(left header): complete the left part of header

---
+++ .history/src/components/Button_20250518202721.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <a className='cta-wrapper'>
+        <div className='cta-group'>
+            
+        </div>
+    </a>
+  )
+}
+
+export default Button
---
+++ .history/src/constants/index_20250518201201.js
@@ -0,0 +1,11 @@
+export const words=[
+    {text: 'Ideas', imgPath: '/image/ideas.svg'},
+    {text: 'Concepts', imgPath: '/image/concepts.svg'},
+    {text: 'Design', imgPath: '/image/design.svg'},
+    {text: 'Code', imgPath: '/image/code.svg'},
+    {text: 'Ideas', imgPath: '/image/ideas.svg'},
+    {text: 'Concepts', imgPath: '/image/concepts.svg'},
+    {text: 'Design', imgPath: '/image/design.svg'},
+    {text: 'Code', imgPath: '/image/code.svg'},
+
+  ]
