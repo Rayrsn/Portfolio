3434483c41d0ef2169a03990866277eea385d67e
Mohammadreza Zarif <m.zariffard1381@gmail.com>
2025-05-21 16:29:56 +0300

    feat(3dmodels): add the 3d models

--- .history/src/components/HeroModels/HeroExperience_20250521152249.jsx
+++ .history/src/components/HeroModels/HeroExperience_20250521152249.jsx
@@ -12,7 +12,7 @@ const HeroExperience = () => {
   return (
     <Canvas camera={{position:[0,0,15] , fov: 45}}>
       <ambientLight intensity={0.2} color="#1a1a40"/>
-      <directionalLight position={[5,5,5]} intensity={5}/>
+      <directionalLight position={[5,5,5]} intensity={1}/>
 
       {/* SETUP THE 3D MODEL */}
       <OrbitControls>
