410c3698a047763093c9a381168bd3214c60b901
Mohammadreza Zarif <m.zariffard1381@gmail.com>
2025-06-04 16:06:54 +0300

    feat(feature card): add the feature card into project

---
+++ .history/src/sections/FeatureCards_20250604160459.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FeatureCards = () => {
+  return (
+    <div className='w-full padding-g-lg'>
+        <div className='mx-auto grid-3-col'>
+            
+        </div>
+
+    </div>
+  )
+}
+
+export default FeatureCards
