5025179e08fcece25370e1d7494cf1a119b41548
Mohammadreza Zarif <m.zariffard1381@gmail.com>
2025-06-05 21:13:31 +0300

    feat(Experience): add the glow for the card boxes

--- .history/src/sections/ExperienceSection_20250605205845.jsx
+++ .history/src/sections/ExperienceSection_20250605205845.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
 import { expCards } from '../constants'
-import GlowCard from '../components/GlowCard'
 
 const ExperienceSection = () => {
   return (
@@ -14,13 +13,7 @@ const ExperienceSection = () => {
                 <div className='relative z-50 xl:space-y-32 space-y-10'>
                   {expCards.map((card)=>(
                     <div key={card.title} className='exp-card-wrapper'>
-                      <div className='xl:w-2/6'>
-                      <GlowCard card={card}>
-                        <div>
-                          <img src={card.imgPath}  alt={card.title} />
-                        </div>
-                      </GlowCard>
-                      </div>
+                      {card.title}
                     </div>
                   ))}
                 </div>
