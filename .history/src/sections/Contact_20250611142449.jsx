af61070bb71eff99d539610f94f827d804bba9dd
Mohammadreza Zarif <m.zariffard1381@gmail.com>
2025-06-11 14:28:23 +0300

    feat(contact): add the contact part and styling the header part

---
+++ .history/src/sections/Contact_20250611142449.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <section className='flext items-center section-padding'>
+        {/* TITLE HEADER */}
+        <div>
+
+        </div>
+
+    </section>
+  )
+}
+
+export default Contact
---
+++ src/sections/Contact.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import TitleHeader from '../components/TitleHeader'
+
+const Contact = () => {
+  return (
+    <section className='flext items-center section-padding'>
+        {/* TITLE HEADER */}
+        <div className='w-full h-full md:px-10 px-5'>
+            <TitleHeader 
+            title= "Get in Touch - Let's Connect"
+            sub="💬 Have questions or ideas? let's talk! "
+            />
+
+        </div>
+
+    </section>
+  )
+}
+
+export default Contact
